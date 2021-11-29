89998a5bc5ed3e095b7ce52dc63957d54a07b89e
Hasni Fodeilla <hasni1.fodeilla@epitech.eu>
2021-11-29 17:41:44 +0100

    commit

--- css_generator.php
+++ css_generator.php
@@ -1,20 +1,17 @@
 <?php
 
 function my_merge_image($first_img_path, $second_img_path){
+
     $size_Img1 = getimagesize($first_img_path);
     $size_Img2 = getimagesize($second_img_path);
-
-    $newImg = imagecreate($width,$height);
-    
-    $width = $size_Img1[0] + $size_Img2[0];
+    $width = max($size_Img1[0],$size_Img2[0]);
     $height = $size_Img1[1] + $size_Img2[1];
+    $newImg = imagecreate($width,$height);
 
     $first_img_path = imagecreatefrompng($first_img_path);
     $second_img_path = imagecreatefrompng($second_img_path);
-
     imagecopy($newImg, $first_img_path, 0, 0, 0, 0, $size_Img1[0], $size_Img1[1]);
-    imagecopy($newImg, $second_img_path, $size_Img1[0], $size_Img1[1], 0, 0, $size_Img2[0], $size_Img2[1]);
+    imagecopy($newImg, $second_img_path, 0, $size_Img1[1], 0, 0, $size_Img2[0], $size_Img2[1]);
     imagepng($newImg, "fusion.png");
 }
-
 my_merge_image("/Users/hasni/delivery/Bootstrap-css_generator/img1.png","/Users/hasni/delivery/Bootstrap-css_generator/img2.png");
